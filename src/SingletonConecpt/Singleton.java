ffad687fad307ede06afd0b4527e4fbbb541ac7c
swathi k <sriswathi1989@gmail.com>
2022-03-07 12:44:17 -0600

    modified file

--- src/SingletonConecpt/Singleton.java
+++ src/SingletonConecpt/Singleton.java
@@ -24,7 +24,7 @@ public class Singleton {
 		if(singleton_instance==null)
 			singleton_instance =new Singleton();
 		return singleton_instance;
-		
+		//this is edited line
 	}
 	
 	public static void main(String[] args) {
